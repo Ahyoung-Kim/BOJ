90f443d8d2c32e57a18cdc18b9589bc384310828
Ahyoung Kim <eileel@naver.com>
2022-02-18 19:32:59 +0900

    단어 길이 재기

---
+++ 2743.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+int main(void) {
+
+	string str;
+
+	getline(cin, str);
+
+	cout << str.size() << endl;
+
+
+	return 0;
+}
