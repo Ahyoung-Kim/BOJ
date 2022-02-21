e718340999d5f2c329b59e29ce8bcf14a6e76b75
Ahyoung Kim <eileel@naver.com>
2022-02-21 13:32:58 +0900

    최소공배수

---
+++ 1934.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<cstring>
+#include<string>
+#include<cstdlib>
+#include<array>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	int r = a % b;
+
+	while (r != 0) {
+		a = b;
+		b = r;
+		r = a % b;
+	}
+	return b;
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+int main(void) {
+	int t;
+	int a, b;
+
+	cin >> t;
+
+	while (t > 0) {
+		cin >> a >> b;
+
+		cout << lcm(a, b) << endl;
+
+		t--;
+	}
+
+	return 0;
+}
