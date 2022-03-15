a253bb897bd216a0cef565345fbe8cf3b1f86ef6
Ahyoung-Kim <eileel@naver.com>
2022-03-15 14:00:24 +0900

    포도주 시식

---
+++ 2156.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<cstdlib>
+
+using namespace std;
+
+int dp[10001] = { 0, };
+int arr[10001];
+
+int main(void) {
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+
+	dp[0] = arr[0] = 0;
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 1]);
+		dp[i] = max(dp[i], dp[i - 2] + arr[i]);
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
