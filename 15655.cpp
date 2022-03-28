9b6a5f018e03f5b0bcb17b60000ea0ddfaece7c3
Ahyoung-Kim <eileel@naver.com>
2022-03-28 17:05:06 +0900

    N과 M(6)

---
+++ 15655.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<cmath>
+
+using namespace std;
+
+int n, m;
+int num[9];
+bool visit[9] = { false, };
+int arr[9];
+
+void dfs(int idx, int depth) {
+
+	if (depth == m - 1) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+
+		return;
+	}
+
+	for (int i = idx + 1; i < n; i++) {
+		if (!visit[i]) {
+			visit[i] = true;
+			arr[depth + 1] = num[i];
+			dfs(i, depth + 1);
+			visit[i] = false;
+		}
+	}
+
+}
+
+int main(void) {
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num[i]);
+	}
+
+	sort(num, num + n);
+
+	for (int i = 0; i < n; i++) {
+		visit[i] = true;
+		arr[0] = num[i];
+		dfs(i, 0);
+		visit[i] = false;
+	}
+
+	return 0;
+}
